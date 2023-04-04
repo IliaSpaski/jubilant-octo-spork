105494c70391831e4cce0522b323145c946858a3
Ilya <iloa.spaski123321@gmail.com>
2023-04-04 18:16:29 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].classList.remove('active-content');
+        tabcontent[i].classList.add('hidden-content');
+
+
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    const activeContent = document.getElementById(cityName);
+    activeContent.style.display = "block";
+
+      activeContent.classList.remove('hidden-content');
+
+      setTimeout(() => {
+        activeContent.classList.add('active-content');
+      },300)
+     
+
+    evt.currentTarget.className += " active";
+  }
+
+  AOS.init();
+
+
+ // preloader
+  window.onload = function () {
+    document.body.classList.add('loaded_hiding');
+    window.setTimeout(function () {
+      document.body.classList.add('loaded');
+      document.body.classList.remove('loaded_hiding');
+    }, 500);
+  }
+
+  
